6fc469b0f7b55396990a480c1628ba5cc1d5eeea
Julio Muller <juliodolago@gmail.com>
2021-07-05 22:39:42 -0300

    Add app-level middlewares

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 require('./database')
 require('express-async-errors')
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 const routes = require('./routes')
 const errorHandler = require('./errors')
 
 const app = express()
 
+app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(routes)
