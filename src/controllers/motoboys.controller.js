549d1de8075f9b49fa176fcdb20741ebd32dfc0c
Julio Muller <juliodolago@gmail.com>
2021-07-06 00:08:17 -0300

    Add JSdocs

--- src/controllers/motoboys.controller.js
+++ src/controllers/motoboys.controller.js
@@ -3,6 +3,12 @@ const { ResourceNotFound } = require('../errors')
 const { motoboysResource } = require('../resources')
 const { Motoboy } = require('../models')
 
+/**
+ * Lista todos os registros de motoboys.Caso o usuário esteja autenticado com
+ * perfil "ASSOC", apenas os registros associados a ele estarão disponíveis.
+ *
+ * @middleware
+ */
 async function index(request, response) {
   const queryFilter = request.auth.role === 'ASSOC'
     ? { where: { associate_id: request.auth.id } }
@@ -14,6 +20,13 @@ async function index(request, response) {
     .json(motoboysResource(motoboys))
 }
 
+/**
+ * Retorna os registros para o motoboy com CPF passado como parâmetro. Caso o
+ * usuário esteja autenticado com perfil "ASSOC" ou "mMOTOBOY", apenas os
+ * registros associados a ele estarão disponíveis.
+ *
+ * @middleware
+ */
 async function show(request, response) {
   const { cpf } = request.params
   const queryFilter = (() => {
@@ -37,16 +50,31 @@ async function show(request, response) {
     .json(motoboysResource(motoboy))
 }
 
+/**
+ * Salva os dados de um novo motoboy.
+ *
+ * @middleware
+ */
 function store(request, response) {
   // TODO: implement
   response.send()
 }
 
+/**
+ * Altera os dados de um motoboy com o ID passado como parâmetro.
+ *
+ * @middleware
+ */
 function update(request, response) {
   // TODO: implement
   response.send()
 }
 
+/**
+ * Exclui o registro de um motoboy com o ID passado como parâmetro.
+ *
+ * @middleware
+ */
 function destroy(request, response) {
   // TODO: implement
   response.send()
