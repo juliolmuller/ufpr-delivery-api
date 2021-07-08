cd59e178d953103e6f3f0fd3af7998f9f8087fcd
Julio Muller <juliodolago@gmail.com>
2021-07-08 00:20:37 -0300

    Create validation for motoboy forms

--- src/middlewares/validate-motoboy.js
+++ src/middlewares/validate-motoboy.js
@@ -1,16 +1,66 @@
-// const yup = require('yup')
-// const cpfUtils = require('../utils')
+/* eslint-disable newline-per-chained-call */
+const { passwordUtils, yup } = require('../utils')
+const { ResourceNotFound } = require('../errors')
+const { Motoboy } = require('../models')
 
-// const newMotoboySchema = yup.object().shape({
-//   name: yup.required().string().max(255),
-//   cpf: yup.required(),
-//   phone: yup.required(),
-//   password: yup.required(),
-//   password_confirmation: yup.required(),
-// })
+const nameSchema = yup.string().required().name()
+const phoneSchema = yup.string().required().phone()
+const cpfSchema = yup.string().required().cpf()
+const newPasswordSchema = yup.string().required().password()
+const oldPasswordSchema = yup.mixed().required()
+  .transform(([oldPassword]) => oldPassword)
+  .test({
+    name: 'old_password',
+    message: 'Senha atual não confere.',
+    async test([oldPassword, motoboyId]) {
+      if (!oldPassword) {
+        return false
+      }
 
-async function validateMotoboy(request, _response, next) {
+      const motoboy = await Motoboy.findByPk(motoboyId)
 
+      if (!motoboy) {
+        throw new ResourceNotFound(`Motoboy com ID '${motoboyId}' não encontrado.`)
+      }
+
+      return passwordUtils.compare(oldPassword, motoboy.password)
+    },
+  })
+
+const newMotoboySchema = yup.object().shape({
+  password: newPasswordSchema,
+  phone: phoneSchema,
+  name: nameSchema,
+  cpf: cpfSchema,
+})
+
+const updateMotoboySchema = yup.object().shape({
+  old_password: oldPasswordSchema,
+  new_password: newPasswordSchema,
+  phone: phoneSchema,
+  name: nameSchema,
+})
+
+function validateMotoboy(validationSchema) {
+  const schema = validationSchema === 'NEW'
+    ? newMotoboySchema
+    : updateMotoboySchema
+
+  return async (request, _response, next) => {
+    request.body = await schema.validate({
+      cpf: request.body.cpf,
+      name: request.body.name,
+      phone: request.body.phone,
+      password: request.body.password,
+      new_password: request.body.new_password,
+      old_password: [
+        request.body.new_password,
+        request.params.id,
+      ], // injeta múltiplos valores para fazer uso no teste
+    }, { abortEarly: false })
+
+    next()
+  }
 }
 
 module.exports = validateMotoboy
--- src/routes/motoboys.router.js
+++ src/routes/motoboys.router.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const { motoboysController } = require('../controllers')
-const { authorize } = require('../middlewares')
+const { authorize, validateMotoboy } = require('../middlewares')
 
 const motoboysRouter = express.Router()
 
 motoboysRouter.get('/', authorize('ADMIN', 'ASSOC'), motoboysController.index)
 motoboysRouter.get('/:cpf', authorize('ADMIN', 'ASSOC', 'MOTOBOY'), motoboysController.show)
-motoboysRouter.post('/', authorize('ASSOC'), motoboysController.store)
-motoboysRouter.put('/:id', authorize('ASSOC', 'MOTOBOY'), motoboysController.update)
+motoboysRouter.post('/', authorize('ASSOC'), validateMotoboy('NEW'), motoboysController.store)
+motoboysRouter.put('/:id', authorize('ASSOC', 'MOTOBOY'), validateMotoboy('UPDATE'), motoboysController.update)
 motoboysRouter.delete('/:id', authorize('ASSOC'), motoboysController.destroy)
 
 module.exports = motoboysRouter
