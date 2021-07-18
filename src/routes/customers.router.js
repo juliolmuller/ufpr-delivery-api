6fe9d85ec7a2e0caca7655392b40ad3928027e68
Fernanda <fer.carvalho153@gmail.com>
2021-07-18 16:37:19 -0300

    feat: add customer controller; add customer validations; add validations to customer router

--- src/routes/customers.router.js
+++ src/routes/customers.router.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const { customersController } = require('../controllers')
-const { authorize } = require('../middlewares')
+const { authorize, validateCustomer } = require('../middlewares')
 
 const customersRouter = express.Router()
 
 customersRouter.get('/', authorize('ADMIN', 'ASSOC'), customersController.index)
 customersRouter.get('/:cnpj', authorize('ADMIN', 'ASSOC'), customersController.show)
-customersRouter.post('/', authorize('ASSOC'), customersController.store)
-customersRouter.put('/:id', authorize('ASSOC'), customersController.update)
+customersRouter.post('/', authorize('ASSOC'), validateCustomer(), customersController.store)
+customersRouter.put('/:id', authorize('ASSOC'), validateCustomer(), customersController.update)
 customersRouter.delete('/:id', authorize('ASSOC'), customersController.destroy)
 
 module.exports = customersRouter
